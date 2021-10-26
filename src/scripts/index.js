1f81c37b39287e2e38031d7efb83e68c47b14bbf
daniel <daniel.zapata.682@gmail.com>
2021-10-25 21:46:26 -0500

    Updated Database

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,11 @@
 // API https://pokeapi.co/api/v2/pokemon/?offset=0&limit=30
+/* const { name, url } = el,
+      pokeInfo = await RestFetch.getData(url),
+      {
+        types: [insideTypes],
+        sprites: { front_default: imgUrl },
+      } = pokeInfo,
+      pokeType = insideTypes.type.name; */
 import RestFetch from "../helpers/RestFetch.js";
 
 const d = document;
@@ -7,20 +14,16 @@ const drawCardTemp = async (data) => {
   const $template = d.querySelector("#card-temp").content,
     $fragment = d.createDocumentFragment(),
     $cardContainer = d.querySelector("#card-cont");
+
   for (const el of data) {
-    const { name, url } = el,
-      pokeInfo = await RestFetch.getData(url),
-      {
-        types: [insideTypes],
-        sprites: { front_default: imgUrl },
-      } = pokeInfo,
-      pokeType = insideTypes.type.name;
+    const { name, imgUrl, pokeType } = el;
 
     $template.querySelector(".card-title").textContent = name;
     $template.querySelector(".card-img").setAttribute("src", imgUrl);
     $template.querySelector(".card-text").textContent = pokeType;
     $fragment.appendChild(d.importNode($template, true));
   }
+
   $cardContainer.appendChild($fragment);
 };
 
